a553fbd7d7e36478908e0a8443857e05879ec3c5
Guest User <Guest@epicodus-26.local>
2019-01-15 16:15:24 -0800

    add js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  // $(".clickable").click(function() {
+    $(".education-hidden").slideDown(1000);
+  // });
+
+  // $(".clickable1").click(function() {
+    $(".jobs-hidden").slideDown(1000);
+  // });
+
+  // $(".clickable2").click(function() {
+    $(".proj-hidden").slideDown(1000);
+  // });
+});
